d62b2eb7c30d6048f7298aa88f0ded3722f10fa1
Ailre <xtramainpp@gmail.com>
2022-06-29 10:10:06 +0900

    error

--- scripts/express.js
+++ scripts/express.js
@@ -9,5 +9,14 @@ app.set('port', port)
 
 app.use('/', express.static(path.join(__dirname, '../dist')))
 
+app.use((err, req, res, next) => {
+    console.log(err)
+    res.status(err.status || 500)
+    res.render('error', {
+        message: err.message,
+        error: {}
+    })
+})
+
 const server = http.createServer(app)
 server.listen(port)

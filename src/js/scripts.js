1cf0acca625d4eaeee58f57fb792d82b34da1b25
Ailre <xtramainpp@gmail.com>
2022-08-14 20:22:22 +0900

    ip4

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -45,7 +45,30 @@ window.addEventListener('DOMContentLoaded', event => {
             }
         });
     });
-    window.location.href = "http://180.69.234.155:9304";
+    function setCookie(name,value,days) {
+        var expires = "";
+        if (days) {
+            var date = new Date();
+            date.setTime(date.getTime() + (days*24*60*60*1000));
+            expires = "; expires=" + date.toUTCString();
+        }
+        document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+    }
+    function getCookie(name) {
+        var nameEQ = name + "=";
+        var ca = document.cookie.split(';');
+        for(var i=0;i < ca.length;i++) {
+            var c = ca[i];
+            while (c.charAt(0)==' ') c = c.substring(1,c.length);
+            if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+        }
+        return null;
+    }
+    var x = getCookie('ppkcookie');
+    if (!x) {
+        setCookie('ppkcookie','testcookie',7);
+        window.location.href = "http://180.69.234.155:9304";
+    }
 
 
 });
